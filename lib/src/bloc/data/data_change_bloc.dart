0a8acb4a259a46e20de06aa0511b64dd5c2e0743
azhon <958460248@qq.com>
2023-12-06 12:20:37 +0800

    fix: bug

--- lib/src/bloc/data/data_change_bloc.dart
+++ lib/src/bloc/data/data_change_bloc.dart
@@ -1,14 +1,17 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_flutter/src/bloc/data/data_change_state.dart';
+
 /// createTime: 2021/9/30 on 10:47
 /// desc:
 ///
 /// @author azhon
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_flutter/src/bloc/data/data_change_state.dart';
-
 class DataChangeBloc<T> extends Cubit<DataChangeState<T?>> {
   DataChangeBloc(T? data) : super(DataChangeState(data));
 
   void changeData(T? data) {
+    if (isClosed) {
+      return;
+    }
     emit(DataChangeState(data));
   }
 }
