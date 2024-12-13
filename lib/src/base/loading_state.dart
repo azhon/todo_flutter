e6e38b60e1f1c7260468ea11f55e3d38424c5f47
azhon <958460248@qq.com>
2024-12-13 09:51:46 +0800

    fix: 优化 bloc中获取context

--- lib/src/base/loading_state.dart
+++ lib/src/base/loading_state.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/material.dart';
+
 /// createTime: 2021/10/19 on 17:15
 /// desc:
 ///
 /// @author azhon
 
 mixin LoadingState {
+  ///获取[BuildContext]
+  BuildContext get buildContext;
+
   ///view层接受bloc层事件
   ///[type]事件类型
   ///[data]事件携带数据
