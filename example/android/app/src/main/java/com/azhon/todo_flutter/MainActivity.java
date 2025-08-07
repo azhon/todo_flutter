4d059d110731b8d680e483bf8b2b2a0724f6d555
azhon <azhon.cn@gmail.com>
2025-08-07 23:34:34 +0800

    feat: update android

---
+++ example/android/app/src/main/java/com/azhon/todo_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.azhon.todo_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- example/android/app/src/main/kotlin/com/azhon/todo_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.azhon.todo_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
